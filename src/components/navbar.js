c641dfb56a2c26181a1a5381888e6d848acea402
naveenc-12 <naveenpadikasu@gmail.com>
2026-07-02 17:31:00 +0530

    Responsive for mobile

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,41 +1,31 @@
 export function navbar() {
     return `
-
 <header class="navbar">
 
     <div class="logo">
         NP
     </div>
 
-    <nav>
+    <div class="menu-toggle" id="menu-toggle">
+        ☰
+    </div>
 
-        <a href="#hero" class="active">
-            Home
-        </a>
+    <nav id="nav-links">
 
-        <a href="#about">
-            About
-        </a>
+        <a href="#hero">Home</a>
 
-        <a href="#skills">
-            Skills
-        </a>
+        <a href="#about">About</a>
 
-        <a href="#experience">
-            Experience
-        </a>
+        <a href="#skills">Skills</a>
 
-        <a href="#projects">
-            Projects
-        </a>
+        <a href="#experience">Experience</a>
 
-        <a href="#contact">
-            Contact
-        </a>
+        <a href="#projects">Projects</a>
+
+        <a href="#contact">Contact</a>
 
     </nav>
 
 </header>
-
 `;
 }
